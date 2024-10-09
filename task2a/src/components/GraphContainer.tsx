30d5c2af3a4d6c8fa2341c00ef13183e2840d7ee
Michael Langbein <michael_langbein@hotmail.de>
2024-10-09 22:14:47 +0200

    responsive, basics

--- task2a/src/components/GraphContainer.tsx
+++ task2a/src/components/GraphContainer.tsx
@@ -4,14 +4,7 @@ import { ReactNode } from 'react';
 
 export default function GraphContainer(props: { children: ReactNode; title: string }) {
   return (
-    <div
-      style={{
-        backgroundColor: 'var(--color-white-1)',
-        borderRadius: '0.5rem',
-        border: '1px solid gray',
-        padding: '0.5rem',
-      }}
-    >
+    <div className="graphContainer">
       <h3>{props.title}</h3>
       {props.children}
     </div>
