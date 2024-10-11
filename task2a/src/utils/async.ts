ed6c5160868b5f83af556def38840444c5eed3ef
michaellangbein <michael_langbein@hotmail.de>
2024-10-11 14:47:20 +0200

    new build

--- task2a/src/utils/async.ts
+++ task2a/src/utils/async.ts
@@ -1,39 +1,39 @@
 import { useRef } from 'react';
 
 export function sleep(timeMs: number) {
-  return new Promise((resolve) => setTimeout(resolve, timeMs));
+    return new Promise((resolve) => setTimeout(resolve, timeMs));
 }
 
 class TypeAhead<T> {
-  private queue: T[] = [];
-  constructor(private delayMs: number, private onDequeue: (queue: T[]) => T[]) {}
+    private queue: T[] = [];
+    constructor(private delayMs: number, private onDequeue: (queue: T[]) => T[]) {}
 
-  public enqueue(e: T) {
-    this.queue.push(e);
-    this.startTimer();
-  }
-
-  private ongoingTimeout?: number;
-  private startTimer() {
-    if (this.ongoingTimeout) {
-      clearTimeout(this.ongoingTimeout);
+    public enqueue(e: T) {
+        this.queue.push(e);
+        this.startTimer();
     }
 
-    this.ongoingTimeout = setTimeout(() => {
-      this.dequeue();
-      this.ongoingTimeout = undefined;
-    }, this.delayMs);
-  }
+    private ongoingTimeout?: number | any;
+    private startTimer() {
+        if (this.ongoingTimeout) {
+            clearTimeout(this.ongoingTimeout);
+        }
 
-  private dequeue() {
-    if (this.queue.length > 0) this.queue = this.onDequeue(this.queue);
-  }
+        this.ongoingTimeout = setTimeout(() => {
+            this.dequeue();
+            this.ongoingTimeout = undefined;
+        }, this.delayMs);
+    }
+
+    private dequeue() {
+        if (this.queue.length > 0) this.queue = this.onDequeue(this.queue);
+    }
 }
 
 export function useTypeAhead<T>(delayMs: number, callback: (queue: T[]) => T[]) {
-  const helper = useRef<TypeAhead<T>>();
-  if (!helper.current) {
-    helper.current = new TypeAhead(delayMs, callback);
-  }
-  return helper.current;
+    const helper = useRef<TypeAhead<T>>();
+    if (!helper.current) {
+        helper.current = new TypeAhead(delayMs, callback);
+    }
+    return helper.current;
 }
